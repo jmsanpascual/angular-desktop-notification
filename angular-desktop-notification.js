4517042b74e925cabb306407573cfd990b204b50
Jhom San Pascual <jm.sanpascual@gmail.com>
2016-09-01 14:42:46 +0800

    Converted the factory to a provider, to expose a configuration function on config phase

--- angular-desktop-notification.js
+++ angular-desktop-notification.js
@@ -3,116 +3,130 @@
 
     window.angular
         .module('ngDesktopNotification', [])
-        .factory('desktopNotification', desktopNotification)
+        .provider('desktopNotification', desktopNotification)
         .constant('PERMISSIONS', {
             DEFAULT: 'default',
             GRANTED: 'granted',
             DENIED: 'denied'
-        })
-        .value('config', {
+        });
+
+    /* @ngInject */
+    function desktopNotification() {
+        var settings = {
             autoClose: true,
             duration: 5,
             showOnPageHidden: false
-        });
+        };
 
-    desktopNotification.$inject = ['$q', '$timeout', 'config', 'PERMISSIONS'];
+        return {
+            config: config,
+            $get: ['$q', '$timeout', 'PERMISSIONS', factory]
+        };
 
-    /* @ngInject */
-    function desktopNotification($q, $timeout, config, PERMISSIONS) {
-        var Notification = window.Notification || window.mozNotification || window.webkitNotification,
-            service = {
-                isSupported: isSupported,
-                currentPermission: currentPermission,
-                requestPermission: requestPermission,
-                show: showNotification,
-                permissions: {
-                    default: PERMISSIONS.DEFAULT,
-                    granted: PERMISSIONS.GRANTED,
-                    denied: PERMISSIONS.DENIED
-                }
-            };
-
-        return service;
-
-        // Public API
-
-        function isSupported() {
-            return ! (typeof Notification === undefined);
-        }
-
-        function currentPermission() {
-            // Wrap with a function, so that we can extend this easily to
-            // support getting permissions using older API versions
-            return Notification.permission;
-        }
-
-        function requestPermission() {
-            if (! isSupported()) $q.reject('Notification API not supported');
-
-            var deferred = $q.defer();
-
-            Notification.requestPermission().then(requestPermission);
-            return deferred.promise;
-
-            // Convert the ES6 promise to angular's $q promise
-            function requestPermission(permission) {
-                if (PERMISSIONS.GRANTED === permission) {
-                    deferred.resolve(permission);
-                } else {
-                    deferred.reject(permission);
+        function config(options) {
+            for (var key in options) {
+                if (settings.hasOwnProperty(key) && options[key] != undefined) {
+                    settings[key] = options[key];
                 }
             }
         }
 
-        function showNotification(title, options) {
-            // Ensures that options is always an object
-            options = options || {};
+        function factory($q, $timeout, PERMISSIONS) {
+            var Notification = window.Notification || window.mozNotification || window.webkitNotification,
+                service = {
+                    isSupported: isSupported,
+                    currentPermission: currentPermission,
+                    requestPermission: requestPermission,
+                    show: showNotification,
+                    permissions: {
+                        'default': PERMISSIONS.DEFAULT,
+                        'granted': PERMISSIONS.GRANTED,
+                        'denied': PERMISSIONS.DENIED
+                    }
+                };
 
-            // Check first if supported, validate arguments, then check if
-            // showOnPageHidden property is set to true, if yes then proceed
-            // on checking if page is visible, lastly check if
-            // notification is disabled by the client
-            if (! isSupported() || ! _isArgsValid(title, options) ||
-                _isPageVisible(options.showOnPageHidden) ||
-                currentPermission() !== PERMISSIONS.GRANTED) return;
+            return service;
 
-            var notification = new Notification(title, options),
-                autoClose = (options.autoClose === undefined) ? config.autoClose : options.autoClose,
-                duration = options.duration || config.duration;
+            // Public API
 
-            notification.onclick = options.onClick;
+            function isSupported() {
+                return ! (typeof Notification === undefined);
+            }
 
-            // If autoClose is set to true, close the notification using the duration
-            if (autoClose) _autoCloseAfter(notification, duration);
+            function currentPermission() {
+                // Wrap with a function, so that we can extend this easily to
+                // support getting permissions using older API versions
+                return Notification.permission;
+            }
 
-            return notification;
-        }
+            function requestPermission() {
+                if (! isSupported()) $q.reject('Notification API not supported');
 
-        // Private functions
+                var deferred = $q.defer();
 
-        function _isArgsValid(title, options) {
-            var isTitleString = angular.isString(title),
-                isOnClickFunction = (! options.onClick || angular.isFunction(options.onClick));
+                Notification.requestPermission().then(requestPermission);
+                return deferred.promise;
 
-            return (isTitleString && isOnClickFunction);
-        }
+                // Convert the ES6 promise to angular's $q promise
+                function requestPermission(permission) {
+                    if (PERMISSIONS.GRANTED === permission) {
+                        deferred.resolve(permission);
+                    } else {
+                        deferred.reject(permission);
+                    }
+                }
+            }
 
-        function _isPageVisible(showOnPageHidden) {
-            // Check both showOnPageHidden parameter and default
-            if (! showOnPageHidden && ! config.showOnPageHidden) return;
+            function showNotification(title, options) {
+                // Ensures that options is always an object
+                options = options || {};
 
-            return ! (
-                window.document.hidden ||
-                // Uncomment when MS support is added
-                // window.document.msHidden ||
-                window.document.mozHidden ||
-                window.document.webkitHidden
-            );
-        }
+                // Check first if supported, validate arguments, then check if
+                // showOnPageHidden property is set to true, if yes then proceed
+                // on checking if page is visible, lastly check if
+                // notification is disabled by the client
+                if (! isSupported() || ! _isArgsValid(title, options) ||
+                    _isPageVisible(options.showOnPageHidden) ||
+                    currentPermission() !== PERMISSIONS.GRANTED) return;
 
-        function _autoCloseAfter(notification, duration) {
-            var durationInMs = duration * 1000;
-            $timeout(notification.close.bind(notification), durationInMs, false);
+                var notification = new Notification(title, options),
+                    autoClose = (options.autoClose === undefined) ? settings.autoClose : options.autoClose,
+                    duration = options.duration || settings.duration;
+
+                notification.onclick = options.onClick;
+
+                // If autoClose is set to true, close the notification using the duration
+                if (autoClose) _autoCloseAfter(notification, duration);
+
+                return notification;
+            }
+
+            // Private functions
+
+            function _isArgsValid(title, options) {
+                var isTitleString = angular.isString(title),
+                    isOnClickFunction = (! options.onClick || angular.isFunction(options.onClick));
+
+                return (isTitleString && isOnClickFunction);
+            }
+
+            function _isPageVisible(showOnPageHidden) {
+                // Check both showOnPageHidden parameter and default
+                if (! showOnPageHidden && ! settings.showOnPageHidden) return;
+
+                return ! (
+                    window.document.hidden ||
+                    // Uncomment when MS support is added
+                    // window.document.msHidden ||
+                    window.document.mozHidden ||
+                    window.document.webkitHidden
+                );
+            }
+
+            function _autoCloseAfter(notification, duration) {
+                var durationInMs = duration * 1000;
+                $timeout(notification.close.bind(notification), durationInMs, false);
+            }
         }
     }
 })(window);
